213b993bacd0c9fdec3ecf2cea07c96f2f5fc4e2
nehabht <ne.bhatti2@icloud.com>
2022-04-20 14:39:10 +0200

    add msg

---
+++ app.js
@@ -0,0 +1,11 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const app = new Vue(
+    {
+        el:"#app",
+
+        data:{
+            msg:"Buondì"
+        }
+    }
+)
